4c47af0789abfc914c97648027546ea087ac8b14
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-05 11:13:41 -0600

    pseudo code added for roadmap

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ function getUserData(id) {
     db2: db2,
     db3: db3,
   };
+
+  // return the promise with resolve and reject
+  // validate the given ID
+  // create specific variables for selected data bases
+  // query each database and assign the created variables to them
+  // create try and catch block
+  // declare obj var to put the data to gather
+  // check the data, if the type of data is not matched assign them to null
+  // if data base has error show error in catch
 }
